487d480cc53147e49b82c746ca556261a984fd2d
IShumihin <shumidze@gmail.com>
2022-12-28 18:20:45 +0400

    bump version

--- django_states/log.py
+++ django_states/log.py
@@ -7,7 +7,7 @@ import sys
 
 from django.db import models
 from django.db.models.base import ModelBase
-from django.utils.encoding import python_2_unicode_compatible
+from six import python_2_unicode_compatible
 from django.utils.translation import ugettext_lazy as _
 from django.conf import settings
 
--- django_states/models.py
+++ django_states/models.py
@@ -17,7 +17,7 @@ __all__ = ('StateMachine', 'StateDefinition', 'StateTransition', 'StateModel')
 
 from django.db import models
 from django.db.models.base import ModelBase
-from django.utils.encoding import python_2_unicode_compatible
+from six import python_2_unicode_compatible
 from django.utils.translation import ugettext_lazy as _
 
 from django_states.machine import StateMachine, StateDefinition, StateTransition
